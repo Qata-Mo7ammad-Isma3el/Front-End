4c792506136a04a3d071c02f5c916792ca870269
Qata-Mo7ammad-Isma3el <imoh40398@gmail.com>
2025-09-19 23:30:21 +0300

    frontend_day_19_JS

---
+++ JS/main.js
@@ -0,0 +1,384 @@
+// #__Introduction And What Is JavaScript ?__# //
+// #__How To Study The Course ?__# //
+// #__Setting Up Environment And Tools__# //
+// #__Work With Chrome Developer Tools__# //
+// #__Where To Put The Code__# //
+// !important note -> always put you <script> in the end of the html page (sequentially)
+
+// #__Comments And Bad Practice__# //
+// single line comment 
+/* multiple line comment */
+// note: im not happy to Study this shit 
+
+// #__Output To Screen__# //
+/*
+    Output To Screen
+    - window.alert()
+    - document.write()
+    - console.log()
+    Syntax
+*/
+window.alert("Hello World!");
+
+
+// #__Console Methods And Styling And Web API__# //
+
+// #__What Is ECMAScript ?__# //
+
+// #__Data Types And typeof Operator__# //
+
+// #__Variables Introduction__# //
+
+// #__Identifiers Name Conventions And Rules__# //
+
+// #__Var, Let, Const Compare__# //
+
+// #__String Syntax And Characters Escape Sequenc__# //es
+
+// #__Concatenation__# //
+
+// #__Template Literals (Template Strings)__# //
+
+// #__Variable And Concatenation Challenge__# //
+
+// #__Arithmetic Operators__# //
+
+// #__Unary Plus And Negation Operators__# //
+
+// #__Type Coercion__# //
+
+// #__Assignment Operators__# //
+
+// #__Operators Challenges__# //
+
+// #__Number__# //
+
+// #__Number Methods__# //
+
+// #__Math Object__# //
+
+// #__Number Challenge__# //
+
+// #__String Methods Part 1__# //
+
+// #__String Methods Part 2__# //
+
+// #__String Methods Part 3__# //
+
+// #__String Challenge__# //
+
+// #__Comparison Operators__# //
+
+// #__Logical Operators__# //
+
+// #__If Conditions__# //
+
+// #__Nested If Conditions__# //
+
+// #__Conditional Ternary Operator__# //
+
+// #__Nullish Coalescing Operator & Logical Or__# //
+
+// #__If Condition Challenge__# //
+
+// #__Switch Statement__# //
+
+// #__Switch And If Condition Challenge__# //
+
+// #__Array Big Introduction__# //
+
+// #__Using Length With Array__# //
+
+// #__Add And Remove From Array__# //
+
+// #__Searching Array__# //
+
+// #__Sorting Array__# //
+
+// #__Slicing Array__# //
+
+// #__Joining Arrays__# //
+
+// #__Array Challenge__# //
+
+// #__Loop – For and The Concept Of Loop__# //
+
+// #__Loop On Sequences__# //
+
+// #__Nested Loops And Trainings__# //
+
+// #__Loop Control – Break, Continue, Label__# //
+
+// #__Loop For – Advanced Example__# //
+
+// #__Practice – Add Products To Page__# //
+
+// #__Loop – While__# //
+
+// #__Loop – Do, While__# //
+
+// #__Loop Challenge__# //
+
+// #__Function Intro And Basic Usage__# //
+
+// #__Function Advanced Example__# //
+
+// #__Function Return Statement And Use Cases__# //
+
+// #__Function Default Parameters__# //
+
+// #__Function Rest Parameters__# //
+
+// #__Practice – Ultimate Function__# //
+
+// #__Random Arguments Function Challenge__# //
+
+// #__Anonymous Function and Use Cases__# //
+
+// #__Return Nested Function__# //
+
+// #__Arrow Function Syntax__# //
+
+// #__Scope – Global And Local__# //
+
+// #__Scope – Block__# //
+
+// #__Scope – Lexical (Static)__# //
+
+// #__Arrow Function Challenge__# //
+
+// #__Higher Order Functions – Map__# //
+
+// #__Higher Order Functions – Map Practice__# //
+
+// #__Higher Order Functions – Filter__# //
+
+// #__Higher Order Functions – Filter Practice__# //
+
+// #__Higher Order Functions – Reduce__# //
+
+// #__Higher Order Functions – Reduce Practice__# //
+
+// #__Higher Order Functions – ForEach And Practice__# //ce
+
+// #__Higher Order Functions Challenge__# //
+
+// #__Object – Introduction__# //
+
+// #__Dot Notation vs Bracket Notation__# //
+
+// #__Nested Object And Advanced Examples__# //
+
+// #__Create Object With New Keyword__# //
+
+// #__This Keyword__# //
+
+// #__Create Object With Create Method__# //
+
+// #__Create Object With Assign Method__# //
+
+// #__What Is DOM ? And Select Elements__# //
+
+// #__Get, Set Elements Content And Attributes__# //
+
+// #__Check Attributes And Examples__# //
+
+// #__Create And Append Elements__# //
+
+// #__Product With Heading And Paragraph Practice__# //
+
+// #__Deal With Children__# //
+
+// #__DOM Events__# //
+
+// #__Validate Form And Prevent Default__# //
+
+// #__Event Simulation – Click, Focus, Blur__# //
+
+// #__ClassList Object and Methods__# //
+
+// #__CSS Styling And Stylesheets__# //
+
+// #__Before, After, Prepend, Append, Remove__# //
+
+// #__DOM Traversing__# //
+
+// #__DOM Cloning__# //
+
+// #__addEventListener__# //
+
+// #__DOM Challenge__# //
+
+// #__What Is BOM ?__# //
+
+// #__Alert, Confirm, Prompt__# //
+
+// #__setTimeout and clearTimeout__# //
+
+// #__setInterval and clearInterval__# //
+
+// #__Window Location Object__# //
+
+// #__Window Open And Close__# //
+
+// #__Window History Object__# //
+
+// #__Scroll, ScrollTo, ScrollBy, Focus, Print, S__# //top
+
+// #__Scroll To Top Using ScrollY Practice__# //
+
+// #__Local Storage__# //
+
+// #__Local Storage Color App Practice__# //
+
+// #__Session Storage And Use Cases__# //
+
+// #__BOM Challenge__# //
+
+// #__Destructuring Arrays Part 1__# //
+
+// #__Destructuring Arrays Part 2__# //
+
+// #__Destructuring Arrays Part 3 Swap Variables__# //
+
+// #__Destructuring Objects Part 1__# //
+
+// #__Destructuring Objects Part 2__# //
+
+// #__Destructuring Function Parameters__# //
+
+// #__Destructuring Mixed Content__# //
+
+// #__Destructuring Challenge__# //
+
+// #__Set Data Type And Methods__# //
+
+// #__Set vs WeakSet And Garbage Collector__# //
+
+// #__Map Data Type vs Object__# //
+
+// #__Map Methods__# //
+
+// #__Map vs WeakMap__# //
+
+// #__Array.from Method__# //
+
+// #__Array.copyWithin Method__# //
+
+// #__Array.some Method__# //
+
+// #__Array.every Method__# //
+
+// #__Spread Syntax And Use Cases__# //
+
+// #__Map And Set Challenge__# //
+
+// #__Intro And What Is Regular Expression?__# //
+
+// #__Regular Expression – Modifiers__# //
+
+// #__Regular Expression – Ranges Part 1__# //
+
+// #__Regular Expression – Ranges Part 2__# //
+
+// #__Regular Expression – Character Classes Part__# // 1
+
+// #__Regular Expression – Character Classes Part__# // 2
+
+// #__Regular Expression – Quantifiers Part 1__# //
+
+// #__Regular Expression – Quantifiers Part 2__# //
+
+// #__Regular Expression – Quantifiers Part 3__# //
+
+// #__Regular Expression – Replace With Pattern__# //
+
+// #__Regular Expression – Form Validation__# //
+
+// #__Test Your Regular Expression And Discussion__# //s
+
+// #__Regular Expression Challenge__# //
+
+// #__OOP Introduction__# //
+
+// #__Constructor Function Introduction__# //
+
+// #__Constructor Function New Syntax__# //
+
+// #__Deal With Properties And Methods__# //
+
+// #__Update Properties & Built In Constructors__# //
+
+// #__Class Static Properties And Methods__# //
+
+// #__Class Inheritance__# //
+
+// #__Class Encapsulation__# //
+
+// #__Prototype Introduction__# //
+
+// #__Add To Prototype Chain__# //
+
+// #__Object Meta Data And Descriptor Part 1__# //
+
+// #__Object Meta Data And Descriptor Part 2__# //
+
+// #__Date And Time Introduction__# //
+
+// #__Get Date And Time__# //
+
+// #__Set Date And Time__# //
+
+// #__Formatting Date And Time__# //
+
+// #__Tracking Operations Time__# //
+
+// #__Generator Function Introduction__# //
+
+// #__Delegate Generator Function__# //
+
+// #__Generate Infinite Numbers__# //
+
+// #__Modules Import And Export__# //
+
+// #__Named vs Default Export And Import All__# //
+
+// #__What Is JSON__# //
+
+// #__JSON Syntax And Compare With JS Object__# //
+
+// #__What Is API__# //
+
+// #__Parse And Stringify__# //
+
+// #__Asynchronous vs Synchronous Programming__# //
+
+// #__Call Stack And Web API__# //
+
+// #__Event Loop And Callback Queue__# //
+
+// #__What Is AJAX And Network Information__# //
+
+// #__Request And Response From Real API__# //
+
+// #__Loop On Data__# //
+
+// #__Callback Hell Or Pyramid Of Doom__# //
+
+// #__Promise Intro And Syntax__# //
+
+// #__Promise – Then, Catch And Finally__# //
+
+// #__Promise And XHR__# //
+
+// #__Fetch API__# //
+
+// #__Promise – All, All Settled, Race__# //
+
+// #__Async And Training__# //
+
+// #__Await And Training__# //
+
+// #__Try, Catch, Finally With Fetch__# //
+
+// #__The End And Advices__# //
