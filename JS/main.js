616b6583bdb8b7036a42d4572d6ca57e5371198a
Qata-Mo7ammad-Isma3el <imoh40398@gmail.com>
2025-12-24 15:37:26 +0300

    frontend_day25

--- JS/main.js
+++ JS/main.js
@@ -1592,20 +1592,330 @@ console.log(calc(10, 20, ...myNumbers)); // 80
 
 // #__Higher Order Functions – Map__ //
 console.log("#------------------------ex_63------------------------#");
+/*
+    • Higher Order Functions
+        ---> is a function that accepts functions as parameters and/or returns a function.
+        - Map
+            --- method creates a new array
+            --- populated with the results of calling a provided function on every element
+            --- in the calling array.
+    • Syntax map(callBackFunction(Element, Index, Array) { }, thisArg)
+        - Element => The current element being processed in the array.
+        - Index => The index of the current element being processed in the array.
+        - Array => The Current Array
+    • Notes
+        - Map Return A New Array
+    • Examples
+        - Anonymous Function
+        - Named Function
+
+*/
+
+let myNums = [1, 2, 3, 4, 5, 6];
+
+let newArray = [];
+
+for (let i = 0; i < myNums.length; i++) {
+    newArray.push(myNums[i] + myNums[i]);
+}
+
+console.log(newArray);
+
+// Same Idea With Map
+
+// let addSelf = myNums.map(function (element, index, arr) {
+//     // console.log(`Current Element => ${element}`);
+//     // console.log(`Current Index => ${index}`);
+//     // console.log(`Array => ${arr}`);
+//     // console.log(`This => ${this}`);
+//     return element + element;
+// }, 10);
+
+// let addSelf = myNums.map((a) => a + a);
+
+// console.log(addSelf);
+
+function addition(ele) {
+    return ele + ele;
+}
+
+var add = myNums.map(addition);
+
+console.log(add);
 // #__Higher Order Functions – Map Practice__# //
 console.log("#------------------------ex_64------------------------#");
+/*
+    • Map
+        - Swap Cases
+        - Inverted Numbers
+        - Ignore Boolean Value
+*/
+
+let swappingCases = "elZERo";
+let invertedNumbers = [1, -10, -20, 15, 100, -30];
+var ignoreNumbers = "Elz123er4o";
+
+// let sw = swappingCases
+//   .split("")
+//   .map(function (ele) {
+//     // Condition ? True : False
+//     return ele === ele.toUpperCase() ? ele.toLowerCase() : ele.toUpperCase();
+//   })
+//   .join("");
+
+// Arrow Function Version
+
+let sw = swappingCases
+    .split("")
+    .map((a) => (a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()))
+    .join("");
+
+console.log(sw);
+
+let inv = invertedNumbers.map(function (ele) {
+    return -ele;
+});
+
+console.log(inv);
+
+var ign = ignoreNumbers
+    .split("")
+    .map(function (ele) {
+        return isNaN(parseInt(ele)) ? ele : "";
+    })
+    .join("");
+
+console.log(ign);
+
+
 // #__Higher Order Functions – Filter__# //
 console.log("#------------------------ex_65------------------------#");
+/*
+    - Filter
+        --- method creates a new array
+        --- with all elements that pass the test implemented by the provided function.
+
+    Syntax filter(callBackFunction(Element, Index, Array) { }, thisArg)
+        - Element => The current element being processed in the array.
+        - Index => The index of the current element being processed in the array.
+        - Array => The Current Array
+*/
+
+// Get Friends With Name Starts With A
+let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
+
+let filterdFriends = friends.filter(function (el) {
+    return el.startsWith("A") ? true : false;
+});
+
+console.log(filterdFriends);
+
+// Get Even Numbers Only
+let numbers = [11, 20, 2, 5, 17, 10];
+
+let evenNumbers = numbers.filter(function (el) {
+    return el % 2 === 0;
+});
+
+console.log(evenNumbers);
+
+// Test Map vs Filter
+
+// let addMap = numbers.map(function (el) {
+//   return el + el;
+// });
+
+// console.log(addMap);
+
+// let addFilter = numbers.filter(function (el) {
+//   return el + el;
+// });
+
+// console.log(addFilter);
+
+
+
 // #__Higher Order Functions – Filter Practice__# //
 console.log("#------------------------ex_66------------------------#");
+/*
+    • Filter
+        - Filter Longest Word By Number
+*/
+
+// Filter Words More Than 4 Characters
+let sentence = "I Love Foood Code Too Playing Much";
+
+let smallWords = sentence
+    .split(" ")
+    .filter(function (ele) {
+        return ele.length <= 4;
+    })
+    .join(" ");
+
+console.log(smallWords);
+
+// Ignore Numbers
+var ignoreNumbers = "Elz123er4o";
+
+var ign = ignoreNumbers
+    .split("")
+    .filter(function (ele) {
+        return isNaN(parseInt(ele));
+    })
+    .join("");
+
+console.log(ign);
+
+// Filter Strings + Multiply
+let mix = "A13BS2ZX";
+
+let mixedContent = mix
+    .split("")
+    .filter(function (ele) {
+        return !isNaN(parseInt(ele));
+    })
+    .map(function (ele) {
+        return ele * ele;
+    })
+    .join("");
+
+console.log(mixedContent);
+
 // #__Higher Order Functions – Reduce__# //
 console.log("#------------------------ex_67------------------------#");
+/*
+    - Reduce
+    --- method executes a reducer function on each element of the array,
+    --- resulting in a single output value.
+
+    Syntax
+    reduce(callBackFunc(Accumulator, Current Val, Current Index, Source Array) { }, initialValue)
+    - Accumulator => the accumulated value previously returned in the last invocation
+    - Current Val => The current element being processed in the array
+    - Index => The index of the current element being processed in the array.
+    ---------- Starts from index 0 if an initialValue is provided.
+    ---------- Otherwise, it starts from index 1.
+    - Array => The Current Array
+*/
+
+let nums = [10, 20, 15, 30];
+
+var add = nums.reduce(function (acc, current, index, arr) {
+    console.log(`Acc => ${acc}`);
+    console.log(`Current Element => ${current}`);
+    console.log(`Current Element Index => ${index}`);
+    console.log(`Array => ${arr}`);
+    console.log(acc + current);
+    console.log(`#############`);
+    return acc + current;
+}, 5);
+
+console.log(add);
+
 // #__Higher Order Functions – Reduce Practice__# //
 console.log("#------------------------ex_68------------------------#");
+/*
+    • Reduce
+        - Longest Word
+        - Remove Characters + Use Reduce
+*/
+
+let theBiggest = ["Bla", "Propaganda", "Other", "AAA", "Battery", "Test", "Propaganda_Two"];
+
+let check = theBiggest.reduce(function (acc, current) {
+    console.log(`Acc => ${acc}`);
+    console.log(`Current Element => ${current}`);
+    console.log(acc.length > current.length ? acc : current);
+    console.log(`#############`);
+    return acc.length > current.length ? acc : current;
+});
+
+console.log(check);
+
+let removeChars = ["E", "@", "@", "L", "Z", "@", "@", "E", "R", "@", "O"];
+
+let finalString = removeChars
+    .filter(function (ele) {
+        return ele !== "@";
+    })
+    .reduce(function (acc, current) {
+        return `${acc}${current}`;
+    });
+
+console.log(finalString);
+
+
 // #__Higher Order Functions – ForEach And Practice__# //ce
 console.log("#------------------------ex_69------------------------#");
+
+document.write(`
+    <hr>
+    <ul>
+        <li class="active">One</li>
+        <li>Two</li>
+        <li>Three</li>
+    </ul>
+    <div class="content">
+        <div>Div One</div>
+        <div>Div Two</div>
+        <div>Div Three</div>
+    </div>`
+)
+
+/*
+    • forEach
+        --- method executes a provided function once for each array element.    
+    • Syntax forEach(callBackFunction(Element, Index, Array) { }, thisArg)
+        - Element => The current element being processed in the array.
+        - Index => The index of the current element being processed in the array.
+        - Array - The Current Array
+    • Note
+        - Doesnt Return Anything [Undefined]
+        - Break Will Not Break The Loop
+*/
+
+let allLis = document.querySelectorAll("ul li");
+let allDivs = document.querySelectorAll(".content div");
+
+allLis.forEach(function (ele) {
+    ele.onclick = function () {
+        // Remove Active Class From All Elements
+        allLis.forEach(function (ele) {
+            ele.classList.remove("active");
+        });
+        // Add Active Class To This Element
+        this.classList.add("active");
+        // Hide All Divs
+        allDivs.forEach(function (ele) {
+            ele.style.display = "none";
+        });
+    };
+});
 // #__Higher Order Functions Challenge__# //
 console.log("#------------------------ex_70------------------------#");
+/*
+    • Higher Order Functions Challenges
+        You Can Use
+        - ,
+        - _
+        - Space
+        - True => 1 => One Time Only In The Code
+        You Cannot Use
+        - Numbers
+        - Letters
+        - You Must Use [Filter + Map + Reduce + Your Knowledge]
+        - Order Is Not Important
+        - All In One Chain
+
+*/
+
+let myString = "1,2,3,EE,l,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,2,0,Z";
+
+let solution = myString.split().map((ele) => console.log(ele));
+
+console.log(solution); // Elzero Web School
+
+
 // #__Object – Introduction__# //
 console.log("#------------------------ex_71------------------------#");
 // #__Dot Notation vs Bracket Notation__# //
