47b59ee832572cc2f36ad31767f5e58583287042
Qata-Mo7ammad-Isma3el <imoh40398@gmail.com>
2025-12-25 14:48:51 +0300

    frontend_day26

--- JS/main.js
+++ JS/main.js
@@ -1915,27 +1915,382 @@ let solution = myString.split().map((ele) => console.log(ele));
 
 console.log(solution); // Elzero Web School
 
-
 // #__Object – Introduction__# //
 console.log("#------------------------ex_71------------------------#");
+
+/*
+    • Object
+        - Intro and What Is Object
+        - Testing Window Object
+        - Accessing Object
+*/
+
+var user = {
+    // Properties
+    theName: "Osama",
+    theAge: 38,
+    // Methods
+    sayHello: function () {
+        return `Hello`;
+    },
+};
+
+console.log(user.theName);
+console.log(user.theAge);
+console.log(user.sayHello());
 // #__Dot Notation vs Bracket Notation__# //
 console.log("#------------------------ex_72------------------------#");
+/*
+    • Object
+        - Dig Deeper
+        - Dot Notation vs Bracket Notation
+        - Dynamic Property Name
+*/
+
+var myVar = "country";
+
+var user = {
+    theName: "Osama",
+    country: "Egypt",
+};
+
+console.log(user.theName);
+console.log(user.country); // user.country
+console.log(user.myVar); // user.country -> undefined
+console.log(user[myVar]); // user.country
+
 // #__Nested Object And Advanced Examples__# //
 console.log("#------------------------ex_73------------------------#");
+/*
+  Object
+  - Nested Object And Trainings
+*/
+
+var available = true;
+
+var user = {
+    name: "Osama",
+    age: 38,
+    skills: ["HTML", "CSS", "JS"],
+    available: false,
+    addresses: {
+        ksa: "Riyadh",
+        egypt: {
+            one: "Cairo",
+            two: "Giza",
+        },
+    },
+    checkAv: function () {
+        if (user.available === true) {
+            return `Free For Work`;
+        } else {
+            return `Not Free`;
+        }
+    },
+    checkAvArrow: () => {
+        return user.available ? `Free For Work` : `Not Free`;
+    }
+};
+
+console.log(user.name);
+console.log(user.age);
+console.log(user.skills);
+console.log(user.skills.join(" | "));
+console.log(user.skills[2]); // Access With Index
+console.log(user.addresses.ksa);
+console.log(user.addresses.egypt.one);
+console.log(user["addresses"].egypt.one);
+console.log(user["addresses"]["egypt"]);
+console.log(user["addresses"]["egypt"]["one"]);
+
+console.log(user.checkAv());
+console.log(user.checkAvArrow());
+
 // #__Create Object With New Keyword__# //
 console.log("#------------------------ex_74------------------------#");
+/*
+    • Object
+        - Create With New Keyword new Object();
+*/
+
+var user = new Object({
+    age: 20,
+});
+
+console.log(user);
+
+user.age = 38;
+user["country"] = "Egypt";
+
+user.sayHello = function () {
+    return `Hello`;
+};
+
+console.log(user);
+console.log(user.age);
+console.log(user.country);
+console.log(user.sayHello());
+
 // #__This Keyword__# //
 console.log("#------------------------ex_75------------------------#");
+/*
+    • Function this Keyword
+        - this Introduction
+        - this Inside Object Method
+        --- When a function is called as a method of an object,
+        --- its this is set to the object the method is called on.
+        - Global Object
+        - Test Variables With Window And This
+        - Global Context
+        - Function Context
+
+    • Search
+        - Strict Mode
+*/
+
+console.log(this);
+console.log(this === window);
+
+myVar = 100;
+
+console.log(window.myVar);
+console.log(this.myVar);
+
+function sayHello() {
+    console.log(this);
+    return this;
+}
+sayHello();
+
+console.log(sayHello() === window);
+
+document.write(`<hr><button id="cl" style="padding: 10px 20px; font-size: 16px; background-color: #008CBA; color: white; border: none; border-radius: 5px; cursor: pointer;">Click Me</button>
+`);
+document.getElementById("cl").onclick = function () {
+    console.log(this);
+};
+
+var user = {
+    age: 38,
+    ageInDays: function () {
+        console.log(this);
+        return this.age * 365;
+    },
+};
+
+console.log(user.age);
+console.log(user.ageInDays());
+
 // #__Create Object With Create Method__# //
 console.log("#------------------------ex_76------------------------#");
+/*
+    • Object
+        - Create Object With Create Method
+*/
+
+var user = {
+    age: 20,
+    doubleAge: function () {
+        return this.age * 2;
+    },
+};
+
+console.log(user);
+console.log(user.age);
+console.log(user.doubleAge());
+
+var obj = Object.create({});
+
+obj.a = 100;
+
+console.log(obj);
+
+var copyObj = Object.create(user);
+
+copyObj.age = 50;
+
+console.log(copyObj);
+console.log(copyObj.age);
+console.log(copyObj.doubleAge());
+
 // #__Create Object With Assign Method__# //
 console.log("#------------------------ex_77------------------------#");
+/*
+    • Object
+        - Create Object With Assign Method  
+*/
+
+var obj1 = {
+    prop1: 1,
+    meth1: function () {
+        return this.prop1;
+    },
+};
+
+var obj2 = {
+    prop2: 2,
+    meth2: function () {
+        return this.prop2;
+    },
+};
+
+var targetObject = {
+    prop1: 100,
+    prop3: 3,
+};
+
+var finalObject = Object.assign(targetObject, obj1, obj2);
+
+finalObject.prop1 = 200;
+finalObject.prop4 = 4;
+
+console.log(finalObject);
+
+var newObject = Object.assign({}, obj1, { prop5: 5, prop6: 6 });
+
+console.log(newObject);
+
 // #__What Is DOM ? And Select Elements__# //
 console.log("#------------------------ex_78------------------------#");
+/*
+    • DOM
+        - What Is DOM
+        - DOM Selectors
+        --- Find Element By ID
+        --- Find Element By Tag Name
+        --- Find Element By Class Name
+        --- Find Element By CSS Selectors
+        --- Find Element By Collection
+        ------ title
+        ------ body
+        ------ images
+        ------ forms
+        ------ links
+*/
+
+document.write(`
+<hr>
+<span class="my-span special">My Span 1</span>
+<span class="my-span">My Span 2</span>
+<p>Hello Paragraph 1</p>
+<p>Hello Paragraph 2</p>
+<div id="my-div">Hello Div</div>
+<form action="">
+    <input type="text" name="one" value="Hello" />
+</form>
+<form action="">
+    <input type="text" name="two" value="Hello" />
+</form>
+<a href="https://google.com">Google</a><br>
+<a href="https://facebook.com">Facebook</a>
+`);
+
+
+
+var myIdElement = document.getElementById("my-div");
+var myTagElements = document.getElementsByTagName("p");
+var myClassElement = document.getElementsByClassName("my-span");
+var myQueryElement = document.querySelector(".my-span");
+var myQueryAllElement = document.querySelectorAll(".my-span");
+
+console.log(myIdElement);
+console.log(myTagElements[1]);
+console.log(myClassElement[1]);
+console.log(myQueryElement);
+console.log(myQueryAllElement[1]);
+
+console.log(document.title);
+console.log(document.body);
+console.log(document.forms[0].one.value);
+console.log(document.links[1].href);
+
 // #__Get, Set Elements Content And Attributes__# //
 console.log("#------------------------ex_79------------------------#");
+/*
+    • DOM [Get / Set Elements Content And Attributes]
+        - innerHTML
+        - textContent
+        - Change Attributes Directly
+        - Change Attributes With Methods
+        --- getAttribute
+        --- setAttribute
+
+    • Search
+        - innerText
+*/
+
+document.write(`
+    <hr>
+    <div class="js">JavaScript <span>Div</span></div><br>
+    <img src="" alt="" /><br>
+    <a class="link" href="#">Google</a>
+`);
+
+var myElement = document.querySelector(".js");
+
+console.log(myElement.innerHTML);
+console.log(myElement.textContent);
+
+myElement.innerHTML = "Text From <span>Main.js</span> File";
+myElement.textContent = "Text From <span>Main.js</span> File";
+
+document.images[0].src = "https://google.com";
+document.images[0].alt = "Alternate";
+document.images[0].title = "Picture";
+document.images[0].id = "pic";
+document.images[0].className = "img";
+
+var myLink = document.querySelector(".link");
+
+console.log(myLink.getAttribute("class"));
+console.log(myLink.getAttribute("href"));
+
+myLink.setAttribute("href", "https://twitter.com");
+myLink.setAttribute("title", "Twitter");
+
 // #__Check Attributes And Examples__# //
 console.log("#------------------------ex_80------------------------#");
+/*
+    • DOM [Check Attributes]
+        - Element.attributes
+        - Element.hasAttribute
+        - Element.hasAttributes
+        - Element.removeAttribute
+*/
+
+
+document.write(`
+    <hr>
+    <div>Div</div>
+    <p class="para" title="Paragraph" data-src="Testing">Paragraph</p>
+`);
+
+var len = document.getElementsByTagName("p").length;
+
+console.log(document.getElementsByTagName("p")[len - 1].attributes);
+
+let myP = document.getElementsByTagName("p")[0];
+
+if (myP.hasAttribute("data-src")) {
+    if (myP.getAttribute("data-src") === "") {
+        myP.removeAttribute("data-src");
+    } else {
+        myP.setAttribute("data-src", "New Value");
+    }
+} else {
+    console.log(`Not Found`);
+}
+
+if (myP.hasAttributes()) {
+    console.log(`Has Attributes`);
+}
+
+if (document.getElementsByTagName("div")[0].hasAttributes()) {
+    console.log(`Has Attributes`);
+} else {
+    console.log(`Div Has No Attributes`);
+}
+
 // #__Create And Append Elements__# //
 console.log("#------------------------ex_81------------------------#");
 // #__Product With Heading And Paragraph Practice__# //
